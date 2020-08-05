173841984d7b2cfac4ace38e9faa247884f5147b
Giles Sandoval <gsandoval2277@gmail.com>
2020-08-04 22:43:50 -0600

    Create functions-return.js

---
+++ functions-return.js
@@ -0,0 +1,11 @@
+// declare a function that has 2 parameters 
+// multiply and determine # of monitors needed
+// check if function works properly using console log
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+
+console.log(numOfMonitors);
