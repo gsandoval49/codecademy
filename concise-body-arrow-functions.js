74fae897c2609a2c5f6e5002746cbb932cd2d50f
Giles Sandoval <gsandoval2277@gmail.com>
2020-08-24 00:44:25 -0600

    Create concise-body-arrow-functions.js
    Let’s refactor plantNeedsWater() to be a concise body. Notice that we’ve already converted the if/else statement to a ternary operator to make the code fit on one line.

---
+++ concise-body-arrow-functions.js
@@ -0,0 +1,9 @@
+// Let’s refactor plantNeedsWater() to be a concise body. Notice that we’ve already converted the if/else statement to a ternary operator to make the code fit on one line.
+
+// Refactor this code to concise body:
+
+// const plantNeedsWater = (day) => {
+//      return day === 'Wednesday' ? true : false;
+// };
+
+const plantNeedsWater = day => day === 'Wednesday' ? true : false; 
