87544d26d76da485662d5d72e38952730b3bff33
Giles Sandoval <gsandoval2277@gmail.com>
2020-09-13 00:04:36 -0600

    Update scope.js

--- scope.js
+++ scope.js
@@ -1,1 +1,12 @@
-// block & scope variables
+// blocked scope variables
+// using const
+
+const isLoggedIn = true;
+
+if (isLoggedIn == true) { 
+    const statusMessage = 'User is logged in.’;
+}
+
+console.log(statusMessage);
+
+// Uncaught ReferenceError: statusMessage is not defined
