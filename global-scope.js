dd038db81a008e0006a5c0a8be4726a3e51c431b
Giles Sandoval <gsandoval2277@gmail.com>
2020-09-13 01:06:16 -0600

    Create global-scope.js

---
+++ global-scope.js
@@ -0,0 +1,17 @@
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+
+// function callMyNighSky = () =>
+// had to use const
+
+const callMyNightSky = () => {
+
+return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+
+};
+
+console.log(callMyNightSky());
+
+//outputs: Night Sky: The Moon, North Star, and The Milky Way
