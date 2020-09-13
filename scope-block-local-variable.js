95387f741edf91d3fdd00131d01574be4cb7c70c
Giles Sandoval <gsandoval2277@gmail.com>
2020-09-13 01:58:38 -0600

    Update scope-block-local-variable.js

--- scope-block-local-variable.js
+++ scope-block-local-variable.js
@@ -1,2 +1,11 @@
 //to show what output works and one that doesn’t. 
 //for a scope block aka local variable
+
+const logVisibleLightWaves = () => {
+    const lightWaves = 'Moonlight';
+    console.log(lightWaves);
+};
+
+
+logVisibleLightWaves(); // OUTPUTS Moonlight
+console.log(lightWaves); // ReferenceError: lightWaves is not defined
